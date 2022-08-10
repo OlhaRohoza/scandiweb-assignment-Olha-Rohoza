6dcf667ee7c39587570306909f19a596f9ad23e3
OlhaRohoza <olha.rohoza@gmail.com>
2022-08-10 18:07:36 +0200

    add persist store - saved cart

--- src/index.js
+++ src/index.js
@@ -4,15 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { legacy_createStore as createStore } from 'redux';
-import { rootReducer } from './redux/reducers';
+import { configureStore } from './redux/store';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
-const store = createStore(rootReducer);
+
+const store = configureStore();
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate
+      loading={<div>Loading...</div>}
+      persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
 
